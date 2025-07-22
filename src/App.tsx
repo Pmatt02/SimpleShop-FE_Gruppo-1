d47b6f459c58bbe9ddd7d5ea22063f73054be512
Alex6477 <stefanachealexflorin@gmail.com>
2025-07-23 01:14:32 +0200

    divisa responsabilità di routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import CartPage from "./pages/CartPage";
-import { CartProvider, useCart } from "./context/CartContext";
 import React from "react";
-import { ProductDetailPage } from "./pages/ProductDetailPage";
-import HomePage from "./pages/HomePage";
 import Navbar from "./components/Navbar";
-import { CheckoutPage } from "./pages/CheckoutPage";
-import { OrderReceivedPage } from "./pages/OrderReceivedPage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppRoutes } from "./routes/AppRoutes";
 
 const App: React.FC = () => {
   return (
     <>
-      <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          {/* <Route path='/gategory/:category' element={<HomePage />}/> */}
-          <Route path='/product/:id' element={<ProductDetailPage />}/>
-          {/* <Route path='/cart' element={<CartPage />}/> */}
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/success" element={<OrderReceivedPage />} />
-        </Routes>
-      </BrowserRouter>
-      {/* <CartProvider>
-          <CartPage></CartPage>
-        </CartProvider> */}
+        <AppRoutes />
     </>
   );
 };
