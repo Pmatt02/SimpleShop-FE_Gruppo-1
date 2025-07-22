c7e28443d804fdd2d3cccc6b4bdabc6b5166e042
Alex6477 <stefanachealexflorin@gmail.com>
2025-07-22 17:04:03 +0200

    sistemato layout ProductDetailPage e aggiunta navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-//import CartPage from './pages/CartPage';
+import { ProductDetailPage } from './pages/ProductDetailPage';
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                {/* Se vuoi la pagina prodotti, importala e usa il nome corretto */}
-                {/* <Route path="/prodotti" element={<ProdottiPage />} /> */}
-                {/* Se invece hai ProductDetailPage o CartPage, usa quelli */}
-
-            </Routes>
-        </Router>
+      <>
+        <ProductDetailPage id={14}/>
+      </>
     );
 };
 
