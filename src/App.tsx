4768f22eee8078fc446b70905988c03e97ae6055
Alex6477 <stefanachealexflorin@gmail.com>
2025-07-22 14:58:10 +0200

    finita pagina di conferma ordine

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { OrderReceivedPage } from './pages/OrderReceivedPage'
 
 function App() {
   return (
     <>
-
+    
     </>
   )
 }
